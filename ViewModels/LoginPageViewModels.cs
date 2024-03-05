6f2bd0ba8b5c75c45f77f8273399f2a8842726cc
arsh052003 <arshmultani0508@gmail.com>
2024-03-04 19:18:47 -0500

    Add project files.

---
+++ ViewModels/LoginPageViewModels.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp3.ViewModels
+{
+    public partial class LoginPageViewModels : BaseViewModel
+    {
+        [ObservableProperty]
+        private string _userName;
+        [ObservableProperty]
+        private string _password;
+
+        [ICommand]
+        public async void Login()
+        {
+
+        }
+
+        
+    }
+}
---
+++ Views/HomePage.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiApp3.Views;
+
+public partial class HomePage : ContentPage
+{
+	public HomePage()
+	{
+		InitializeComponent();
+	}
+}
