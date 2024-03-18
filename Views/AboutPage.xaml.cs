810e56b6cdcb0e8fe0685a4fc8ee9da770bad2a8
arsh052003 <arshmultani0508@gmail.com>
2024-03-17 20:02:50 -0400

    Dekra - Added new views 1. AboutPage: 2. ContactPage: 3. Homepage:

---
+++ Views/AboutPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiApp3.Views;
+
+public partial class AboutPage : ContentPage
+{
+	public AboutPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Views/ContactPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiApp3.Views;
+
+public partial class ContactPage : ContentPage
+{
+	public ContactPage()
+	{
+		InitializeComponent();
+	}
+}
