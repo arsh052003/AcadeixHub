c8c9d0f5906e56522abcd163702da5abc5d31bf5
arsh052003 <arshmultani0508@gmail.com>
2024-03-22 19:29:15 -0400

    Arshdeep - Added UserInfo class to store user credentials in Models folder. This class includes properties for username and password, providing a structured way to manage user information.

--- Models/UserInfo.cs
+++ Models/UserInfo.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.VisualBasic;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,8 +8,7 @@ using System.Threading.Tasks;
 namespace MauiApp3.Models
 {
     public class UserInfo
-    {
-        public int UserID { get; set; }
+    { 
         public string UserName { get; set; }
 
         public string Password { get; set; }
