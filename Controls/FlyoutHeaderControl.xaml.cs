aa7514651b3b4475fd44796d7aec4629bcca766f
arsh052003 <arshmultani0508@gmail.com>
2024-03-31 00:02:19 -0400

    Arshdeep Kaur -  This update tweaks the FlyoutHeaderControl class within the MauiApp3.Controls namespace. This class, responsible for organizing layout in our Maui application, now checks for user details during initialization. If user details are available, it performs further actions, although specifics aren't outlined here. These adjustments likely improve how the app's flyout navigation behaves, making it smoother or adding extra features when user details are present. Overall, it enhances the app's usability for a better user experience.

---
+++ Controls/FlyoutHeaderControl.xaml.cs
@@ -0,0 +1,12 @@
+namespace MauiApp3.Controls;
+
+public partial class FlyoutHeaderControl : StackLayout
+{
+	public FlyoutHeaderControl()
+	{
+		InitializeComponent();
+        if (App.UserDetails != null)
+        {
+        }
+    }
+}
