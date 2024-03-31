1204f1214cc57e3a639a40c8621158c75102ab75
arsh052003 <arshmultani0508@gmail.com>
2024-03-31 00:09:30 -0400

    Dekra -  StudentPage and TeacherPage are partial classes inheriting from ContentPage, designed for displaying content tailored to student and teacher users respectively. LoadingPage is a ContentPage designed to display loading content, with its ViewModel linked for data handling. These additions enhance the app's structure, providing specific pages for different user roles and improving the user experience by ensuring relevant content and functionality are readily accessible.

---
+++ Views/Homepage/StudentPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiApp3.Views.Homepage;
+
+public partial class StudentPage : ContentPage
+{
+	public StudentPage()
+	{
+        InitializeComponent();
+    }
+}
---
+++ Views/Homepage/TeacherPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiApp3.Views.Homepage;
+
+public partial class TeacherPage : ContentPage
+{
+	public TeacherPage()
+	{
+        InitializeComponent();
+	}
+}
